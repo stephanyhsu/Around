6c82e6c84de20b43d4f5ebb3a6dcc2ad06790a92
Stephany <nico96851@gmail.com>
2017-11-19 15:17:07 -0800

    change external IP

--- service/main.go
+++ service/main.go
@@ -34,7 +34,7 @@ const (
 	PROJECT_ID = "rosy-timer-185822"
 	BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://104.196.189.127:9200"
+	ES_URL = "http://35.196.123.197:9200"
 )
 
 
